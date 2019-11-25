8c1df8044fcbc283c4cf5dde88170879134d2af3
Maxime Thonneau <thonneaum@gmail.com>
2019-11-25 15:39:57 +0100

    JSON

--- src/main/Parse.java
+++ src/main/Parse.java
@@ -11,44 +11,49 @@ public class Parse {
     public static void main(String[] args) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            // write your code here !
+        // TODO : get the root from the file JSON_WEATHER_PATH
+		JsonNode root = objectMapper.readTree(new File(JSON_WEATHER_PATH));
 
-            // TODO : get the root from the file JSON_WEATHER_PATH
-            JsonNode root = null;
+		// TODO : get the value of "name" attribute
+		String cityName = root.get("name").asText();
 
-            // TODO : get the value of "name" attribute
-            String cityName = null;
+		// TODO : get the "lat" and "lon" values of the "coord"
+		JsonNode coordObject = root.get("coord");
+		
+		Double cityLatitude = coordObject.get("lat").asDouble();
+		Double cityLongitude = coordObject.get("lon").asDouble();
 
-            // TODO : get the "lat" and "lon" values of the "coord"
-            Double cityLatitude = null;
-            Double cityLongitude = null;
+		// TODO : get the "wind" attribute as an Wind object
+		JsonNode windObject = root.get("wind");
+		
+		Wind wind = new Wind();
+		wind.setSpeed(windObject.get("speed").asDouble());
+		wind.setDeg(windObject.get("deg").asDouble());
+		
+		
+		// TODO : get the "weather" attribute as an array of Weather objects
+		Weather[] weathers = objectMapper.convertValue(root.get("weather"), Weather[].class);
 
-            // TODO : get the "wind" attribute as an Wind object
-            Wind wind = null;
+		// Don't touch this !
+		System.out.printf("City name: %s%n", cityName);
+		System.out.printf("City latitude: %s%n", cityLatitude);
+		System.out.printf("City longitude: %s%n", cityLongitude);
+		System.out.printf("Wind infos: %s%n", wind.toString());
+		for (Weather weather : weathers) {
+		    System.out.printf("Weather infos: %s%n", weather.toString());
+		}
+		/*
+		    Expected result :
 
-            // TODO : get the "weather" attribute as an array of Weather objects
-            Weather[] weathers = {};
-
-            // Don't touch this !
-            System.out.printf("City name: %s%n", cityName);
-            System.out.printf("City latitude: %s%n", cityLatitude);
-            System.out.printf("City longitude: %s%n", cityLongitude);
-            System.out.printf("Wind infos: %s%n", wind.toString());
-            for (Weather weather : weathers) {
-                System.out.printf("Weather infos: %s%n", weather.toString());
-            }
-            /*
-                Expected result :
-
-                City name: London
-                City latitude: 51.51
-                City longitude: -0.13
-                Wind infos: src.main.Wind{speed=4.1, deg=80.0}
-                Weather infos: src.main.Weather{id=300, main='Drizzle', description='light intensity drizzle', icon='09d'}
-                Weather infos: src.main.Weather{id=800, main='Clear', description='clear sky', icon='01n'}
-            */
-        } catch (IOException e) {
-            e.printStackTrace();
+		    City name: London
+		    City latitude: 51.51
+		    City longitude: -0.13
+		    Wind infos: src.main.Wind{speed=4.1, deg=80.0}
+		    Weather infos: src.main.Weather{id=300, main='Drizzle', description='light intensity drizzle', icon='09d'}
+		    Weather infos: src.main.Weather{id=800, main='Clear', description='clear sky', icon='01n'}
+		*/
+        }catch(IOException e) {
+        	e.printStackTrace();
         }
     }
 }
